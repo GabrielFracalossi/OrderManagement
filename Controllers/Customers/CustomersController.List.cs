5636d5c677a175aec072e19f02a5e6840e663cb4
Gabriel Fracalossi <gabriel.fracalossi@azurebrasil.cloud>
2025-02-13 02:12:17 -0300

    feat: Create GET and POST methods in OrdersController

--- Controllers/Customers/CustomersController.List.cs
+++ Controllers/Customers/CustomersController.List.cs
@@ -14,24 +14,13 @@ namespace OrderManagement.Controllers.Customers
             return context.Customers.ToList();
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<CustomersViewModel> GetCustomerById(int id)
-        {
-            var customer = context.Customers.Find(id);
-            if (customer == null)
-            {
-                return NotFound();
-            }
-            return customer;
-        }
-
         [HttpPost]
         public ActionResult<CustomersViewModel> CreateCustomer(CustomersViewModel customer)
         {
             context.Customers.Add(customer);
             context.SaveChanges();
 
-            return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+            return CreatedAtAction(nameof(GetCustomers), new { id = customer.Id }, customer);
         }
     }
 }
--- Controllers/Orders/OrdersController.List.cs
+++ Controllers/Orders/OrdersController.List.cs
@@ -1,6 +1,25 @@
-﻿namespace OrderManagement.Controllers.Orders
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderManagement.Data;
+using OrderManagement.Controllers.Orders.Models;
+namespace OrderManagement.Controllers.Orders
 {
-    public partial class OrdersController
+    [Route("api/orders")]
+    [ApiController]
+    public partial class OrdersController(OrderContext context) : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<OrdersViewModel>> GetOrders()
+        {
+            return context.Orders.Include(o => o.Customer).Include(o => o.Items).ThenInclude(i => i.Product).ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<OrdersViewModel> CreateOrder(OrdersViewModel order)
+        {
+            context.Orders.Add(order);
+            context.SaveChanges();
+            return CreatedAtAction(nameof(GetOrders), new { id = order.Id }, order);
+        }
     }
 }
