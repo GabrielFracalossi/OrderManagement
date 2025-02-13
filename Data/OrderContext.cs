e7c1640b954e8b5349eeff6963bafcabf144e61b
Gabriel Fracalossi <gabriel.fracalossi@azurebrasil.cloud>
2025-02-13 01:44:39 -0300

    feat: Create file OrderContext.cs

---
+++ Data/OrderContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Controllers.Customers.Models;
+using OrderManagement.Controllers.Products.Models;
+using OrderManagement.Controllers.Orders.Models;
+
+namespace OrderManagement.Data
+{
+    public class OrderContext(DbContextOptions<OrderContext> options) : DbContext(options)
+    {
+        public DbSet<ProductsViewModel> Products { get; set; }
+        public DbSet<CustomersViewModel> Customers { get; set; }
+        public DbSet<OrdersViewModel> Orders { get; set; }
+        public DbSet<OrderItemViewModel> OrderItems { get; set; }
+    }
+}
