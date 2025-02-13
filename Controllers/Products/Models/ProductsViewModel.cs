78af8aafb5d55aef04a0cd2e2381bab7bd260115
Gabriel Fracalossi <gabriel.fracalossi@azurebrasil.cloud>
2025-02-13 02:39:14 -0300

    feat: Add format to Products create view

--- Controllers/Products/Models/ProductsViewModel.cs
+++ Controllers/Products/Models/ProductsViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace OrderManagement.Controllers.Products.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderManagement.Controllers.Products.Models
 {
     public class ProductsViewModel
     {
         public Guid Id { get; set; }
         public required string Name { get; set; }
+
+        [Range(0.01, double.MaxValue)]
         public decimal Price { get; set; }
         public int Stock { get; set; }
     }
